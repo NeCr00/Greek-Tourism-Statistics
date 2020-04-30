49d6e73683c248439fb0d27ba8fecd7d88ee906b
Giannis Christodoulakos <giannisxristodou@hotmail.com>
2020-04-30 12:17:23 +0300

    up

--- DataExtraction.py
+++ DataExtraction.py
@@ -1,7 +1,9 @@
 from itertools import product
 import xlrd
 import os
+import json
 
+info_list = []
 #gia kathe etos vriskoume to sunoliko plithismo touristwn apo oles tis xwres
 for year in range(2011,2014):
     #anoigma tou arxeiou tou etous year
@@ -17,5 +19,11 @@ for year in range(2011,2014):
             if('Ιανουάριος-Δεκέμβριος' in str(sheet.cell_value(row,col))):
                 flag = 1
             if(sheet.cell_value(row,col) == 'ΓΕΝΙΚΟ ΣΥΝΟΛΟ' and flag):
-                print(round(sheet.cell_value(row,col+2)))
+                #print(year)
+                tourists = round(sheet.cell_value(row,col+2))
+                info_list.append({'year':year,'total_tourists':tourists})
+
+for x in info_list:
+    print(x.get('year'),x.get('total_tourists'))
+    
 
